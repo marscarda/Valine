ee717f15aa993bdce3b3de988037c8081a9540fc
marscarda <52551131+marscarda@users.noreply.github.com>
2020-10-04 03:17:33 -0300

    Response Work

--- src/java/valine/responsecast/ApiCastFieldResponses.java
+++ src/java/valine/responsecast/ApiCastFieldResponses.java
@@ -8,7 +8,7 @@ import mars.jsonsimple.JsonPair;
 import methionine.AppException;
 import methionine.auth.Session;
 import tryptophan.epsilon.fieldcast.FieldCollector;
-import tryptophan.survey.FieldCast;
+import tryptophan.epsilon.fieldcast.FieldCast;
 import valine.ApiAlpha;
 import valine.FlowAlpha;
 //***************************************************************************
