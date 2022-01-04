a281dd4133a3cbd4c7ae9866c43e889b90d73bff
marscarda <52551131+marscarda@users.noreply.github.com>
2022-01-04 17:47:12 -0300

    DB Refactoring Looks fine

--- src/java/valine/ContextListener.java
+++ src/java/valine/ContextListener.java
@@ -4,16 +4,8 @@ import java.util.Properties;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import methionine.finance.BillingLambda;
 import methionine.Electra;
-import methionine.auth.AuthLamda;
-import methionine.project.ProjectLambda;
 import histidine.AurigaObject;
-import tryptophan.design.DesignTabs;
-import threonine.map.QueryMapTabs;
-import threonine.universe.UniverseAtlas;
-import tryptophan.trial.TrialTabs;
-import tryptophan.sample.SampleTabs;
 //****************************************************************************
 @WebListener
 public class ContextListener implements ServletContextListener {
@@ -21,11 +13,12 @@ public class ContextListener implements ServletContextListener {
     //These properties are directly initialized into Electra.
     static final String PROP_MASTER_DBHOST = "db_master_host";
     static final String PROP_SLAVE_DBHOST = "db_slave_host";
-    static final String PROP_DBUSER = "db_user";
-    static final String PROP_DBPASS = "db_pass";
     static final String PROP_DBMASTER = "db_ismaster";
     //-----------------------------------------------------------------------
     boolean ismaster = false;
+    //-----------------------------------------------------------------------
+    static final String DBUSER = "valine";
+    static final String DBPASS = "jkdy89sreft45trt5trtg5elkwqscf";
     //***********************************************************************
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -65,8 +58,6 @@ public class ContextListener implements ServletContextListener {
         //-----------------------------------------------------
         String masterhost = properties.getProperty(PROP_MASTER_DBHOST);
         String slavehost = properties.getProperty(PROP_SLAVE_DBHOST);
-        String user = properties.getProperty(PROP_DBUSER);
-        String pass = properties.getProperty(PROP_DBPASS);
         String master = properties.getProperty(PROP_DBMASTER);
         //-----------------------------------------------------
         if (masterhost == null) System.out.println("Warning: Property " + PROP_MASTER_DBHOST + " Not set");
@@ -78,18 +69,15 @@ public class ContextListener implements ServletContextListener {
         }
         Electra.setDefaultSlaveHost(slavehost);
         //-----------------------------------------------------
-        if (user == null) System.out.println("Warning: Property " + PROP_DBUSER + " Not set");
-        else Electra.setDefaultDBUser(user);
-        //-----------------------------------------------------
-        if (pass == null) System.out.println("Warning: Property " + PROP_DBPASS + " Not set");
-        else Electra.setDefaultDBPass(pass);
-        //-----------------------------------------------------
         //Below. If this is the master DB server.
         if (master == null) System.out.println("Warning: Property " + PROP_DBMASTER + " Not set");
         else {
             ismaster = master.equalsIgnoreCase("Y");
             Electra.setDefaultMaster(ismaster);
         }
+        //-----------------------------------------------------
+        Electra.setDefaultDBUser(DBUSER);
+        Electra.setDefaultDBPass(DBPASS);
         //-----------------------------------------------------
         System.out.println("Electra initialized");
         //-----------------------------------------------------
@@ -99,197 +87,99 @@ public class ContextListener implements ServletContextListener {
      * Ensures all tables are created.
      */
     private void ensureTables () {
-        //----------------------------------------------------------
-        System.out.println("Ensuring tables");
-        if (!ismaster) return;
-        //----------------------------------------------------------
-        Electra electra = new Electra();
-        //----------------------------------------------------------
-        //Ensures the auth database.
-        if (AurigaObject.dbauth != null) {
-            AuthLamda auth = new AuthLamda();
-            auth.setElectraObject(electra);
-            auth.setDataBaseName(AurigaObject.dbauth);
-            try { 
-                auth.ensureTables();
-                System.out.println("Tables in " + AurigaObject.dbauth + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbauth + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        //*******************************************************************
+        if (ismaster) { System.out.println("Ensuring tables"); }
+        else {
+            System.out.println("This is not a master");
+            System.out.println("Ensuring tables not applying");
+            return;
         }
-        //----------------------------------------------------------
-        //We ensure Project tables exists 
-        if (AurigaObject.dbauth != null) {
-            ProjectLambda workteam = new ProjectLambda();
-            workteam.setElectraObject(electra);
-            workteam.setDataBaseName(AurigaObject.dbauth);
-            try { 
-                workteam.ensureTables();
-                System.out.println("Workteam Tables in " + AurigaObject.dbauth + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbauth + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        //*******************************************************************
+        AurigaObject auriga = new AurigaObject();
+        //*******************************************************************
+        try { 
+            auriga.getAuthLambda().ensureTables(); 
+            System.out.println("Tables in "+ methionine.DBNames.AUTHDATA + " Ensured");
         }
-        //----------------------------------------------------------
-        //We ensure Billing tables exist 
-        if (AurigaObject.dbbilling != null) {
-            BillingLambda billing = new BillingLambda();
-            billing.setElectraObject(electra);
-            billing.setDataBaseName(AurigaObject.dbbilling);
-            try { 
-                billing.ensureTables();
-                System.out.println("Billing Tables in " + AurigaObject.dbbilling + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbbilling + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + methionine.DBNames.AUTHDATA + " tables error");
+            System.out.println(e.getMessage());
         }
-        //----------------------------------------------------------
+        //===================================================================
+        try {
+            auriga.projectAtlas().ensureTables(); 
+            System.out.println("Tables in "+ methionine.DBNames.PROJECT + " Ensured");
+        }
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + methionine.DBNames.PROJECT + " tables error");
+            System.out.println(e.getMessage());
+        }
+        //===================================================================
+        try {
+            auriga.messagingAtlas().ensureTables(); 
+            System.out.println("Tables in "+ methionine.DBNames.MESSAGING + " Ensured");
+        }
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + methionine.DBNames.MESSAGING + " tables error");
+            System.out.println(e.getMessage());
+        }
+        //===================================================================
+        try {
+            auriga.getBillingLambda().ensureTables(); 
+            System.out.println("Tables in "+ methionine.DBNames.FINANCE + " Ensured");
+        }
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + methionine.DBNames.FINANCE + " tables error");
+            System.out.println(e.getMessage());
+        }        
+        //*******************************************************************
         //We ensure Universe tables exists
-        if (AurigaObject.dbuniverse != null) {
-            UniverseAtlas univ = new UniverseAtlas();
-            univ.setElectraObject(electra);
-            univ.setDataBaseName(AurigaObject.dbuniverse);
-            try { 
-                univ.ensureTables();
-                System.out.println("Tables in " + AurigaObject.dbuniverse + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbuniverse + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        try {
+            auriga.getUniverseAtlas().ensureTables();
+            System.out.println("Tables in "+ threonine.DBNames.UNIVERSE + " Ensured");
         }
-        //----------------------------------------------------------
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + threonine.DBNames.UNIVERSE + " tables error");
+            System.out.println(e.getMessage());
+        }
+        //===================================================================   
         //We ensure Maps tables exists
-        if (AurigaObject.dbmaps != null) {
-            QueryMapTabs maps = new QueryMapTabs();
-            maps.setElectraObject(electra);
-            maps.setDataBaseName(AurigaObject.dbmaps);
-            try { 
-                maps.ensureTables();
-                System.out.println("Tables in " + AurigaObject.dbmaps + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbmaps + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        try {
+            auriga.getMapsLambda().ensureTables();
+            System.out.println("Tables in "+ threonine.DBNames.MAPPING + " Ensured");
         }
-        //----------------------------------------------------------
-        if (AurigaObject.dbdesign != null) {
-            DesignTabs design = new DesignTabs();
-            design.setElectraObject(electra);
-            design.setDataBaseName(AurigaObject.dbdesign);
-            try { 
-                design.ensureTables();
-                System.out.println("Tables in " + AurigaObject.dbdesign + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbdesign + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + threonine.DBNames.MAPPING + " tables error");
+            System.out.println(e.getMessage());
         }
-        //----------------------------------------------------------
-        if (AurigaObject.dbsample != null) {
-            SampleTabs sample = new SampleTabs();
-            sample.setElectraObject(electra);
-            sample.setDataBaseName(AurigaObject.dbsample);
-            try { 
-                sample.ensureTables();
-                System.out.println("Tables in " + AurigaObject.dbsample + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbsample + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        //*******************************************************************
+        try {
+            auriga.getDesignLambda().ensureTables();
+            System.out.println("Tables in "+ tryptophan.DBNames.DESIGN + " Ensured");
         }
-        //----------------------------------------------------------
-        if (AurigaObject.dbspace != null) {
-            TrialTabs environment = new TrialTabs();
-            environment.setElectraObject(electra);
-            environment.setDataBaseName(AurigaObject.dbspace);
-            try { 
-                environment.ensureTables();
-                System.out.println("Tables in " + AurigaObject.dbspace + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + AurigaObject.dbspace + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + tryptophan.DBNames.DESIGN + " tables error");
+            System.out.println(e.getMessage());
         }
-        //----------------------------------------------------------
-        /*
-        if (LifeTimeValues.dbsample != null) {
-            DesignTabs design = new DesignTabs();
-            design.setElectraObject(electra);
-            design.setDataBaseName(LifeTimeValues.dbsample);
-            try { 
-                design.ensureTables();
-                System.out.println("Tables in " + LifeTimeValues.dbsample + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + LifeTimeValues.dbsample + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        //===================================================================   
+        try {
+            auriga.getSampleLambda().ensureTables();
+            System.out.println("Tables in "+ tryptophan.DBNames.SAMPLE + " Ensured");
         }
-        */
-        //----------------------------------------------------------
-        // Below here deprecated.
-        //----------------------------------------------------------
-        /*
-        if (LifeTimeValues.dbsurvey != null) {
-            DesignTabs variable = new DesignTabs();
-            variable.setElectraObject(electra);
-            variable.setDataBaseName(LifeTimeValues.dbsurvey);
-            try { 
-                variable.ensureTables();
-                System.out.println("Tables in " + LifeTimeValues.dbsurvey + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + LifeTimeValues.dbsurvey + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + tryptophan.DBNames.SAMPLE + " tables error");
+            System.out.println(e.getMessage());
         }
-        */
-        //----------------------------------------------------------
-        /*
-        if (LifeTimeValues.dbmetrics != null) {
-            MetricQueryInterface metric = new MetricQueryInterface();
-            metric.setElectraObject(electra);
-            metric.setDataBaseName(LifeTimeValues.dbmetrics);
-            try { 
-                metric.ensureTables();
-                System.out.println("Tables in " + LifeTimeValues.dbmetrics + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + LifeTimeValues.dbmetrics + " tables error");
-                System.out.println(e.getMessage());
-            }            
+        //===================================================================   
+        try {
+            auriga.getTrialAtlas().ensureTables();
+            System.out.println("Tables in "+ tryptophan.DBNames.TRIAL + " Ensured");
         }
-        */
-        //----------------------------------------------------------
-        /*
-        if (LifeTimeValues.dbsurvey != null) {
-            SurveyTabs survey = new SurveyTabs();
-            survey.setElectraObject(electra);
-            survey.setDataBaseName(LifeTimeValues.dbsurvey);
-            try { 
-                survey.ensureTables();
-                System.out.println("Tables in " + LifeTimeValues.dbsurvey + " Ensured");
-            }
-            catch (Exception e) {
-                System.out.println("ERROR: Ensuring " + LifeTimeValues.dbsurvey + " tables error");
-                System.out.println(e.getMessage());
-            }            
-        }
-        */
-        //----------------------------------------------------------
-        electra.disposeDBConnection();
-        //----------------------------------------------------------
+        catch (Exception e) {
+            System.out.println("ERROR: Ensuring " + tryptophan.DBNames.TRIAL + " tables error");
+            System.out.println(e.getMessage());
+        }        
+        //*******************************************************************
     }
     //***********************************************************************
 }
--- src/java/valine/LifeTimeValues.java
+++ src/java/valine/LifeTimeValues.java
@@ -1,32 +1,11 @@
 package valine;
 //**************************************************************************
 import java.util.Properties;
-import methionine.DataBaseName;
-import histidine.AurigaObject;
 //**************************************************************************
 public class LifeTimeValues {
     //*****************************************************************
-    //DATABASES PROPERTIES NAME.
-    static final String PROP_DATABASE_USERS = "db_users";
-    static final String PROP_DATABASE_BILLING = "db_finance";
-    static final String PROP_DATABASE_UNIVERSE = "db_universe";
-    static final String PROP_DATABASE_MAPS = "db_maps";
-    static final String PROP_DATABASE_DESIGN = "db_design";
-    static final String PROP_DATABASE_SAMPLE = "db_sample";
-    static final String PROP_DATABASE_ENVIRONMENT = "db_environment";
-    //----------------------------------
-    //@Deprecated
-    //static final String PROP_DATABASE_METRICS = "db_metrics";
-    //@Deprecated
-    //static final String PROP_DATABASE_SURVEY = "db_survey";
-    //=================================================================
     static final String PROP_USING_SSL = "using_ssl";
     //*****************************************************************
-    //@Deprecated
-    //public static String dbmetrics = PROP_DATABASE_METRICS;
-    //@Deprecated
-    //public static String dbsurvey = PROP_DATABASE_SURVEY;
-    //=================================================================
     public static boolean usingssl = false;
     //*****************************************************************
     public static void initValues (Properties props) {
@@ -37,53 +16,7 @@ public class LifeTimeValues {
         if (aux == null) System.out.println("Warning: Property " + PROP_USING_SSL + " Not set");
         else usingssl = aux.equalsIgnoreCase("Y");
         //========================================================
-        //Databases
-        aux = props.getProperty(PROP_DATABASE_USERS);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_USERS + " Not set");
-        else AurigaObject.dbauth = aux;
-        //--------------------------------------------------------
-        aux = props.getProperty(PROP_DATABASE_BILLING);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_BILLING + " Not set");
-        else AurigaObject.dbbilling = aux;
-        //--------------------------------------------------------
-        aux = props.getProperty(PROP_DATABASE_UNIVERSE);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_UNIVERSE + " Not set");
-        else AurigaObject.dbuniverse = aux;
-        //--------------------------------------------------------
-        aux = props.getProperty(PROP_DATABASE_MAPS);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_MAPS + " Not set");
-        else AurigaObject.dbmaps = aux;
-        //--------------------------------------------------------
-        aux = props.getProperty(PROP_DATABASE_MAPS);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_MAPS + " Not set");
-        else AurigaObject.dbmaps = aux;
-        //--------------------------------------------------------
-        aux = props.getProperty(PROP_DATABASE_DESIGN);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_DESIGN + " Not set");
-        else AurigaObject.dbdesign = aux;
-        //--------------------------------------------------------
-        aux = props.getProperty(PROP_DATABASE_SAMPLE);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_SAMPLE + " Not set");
-        else AurigaObject.dbsample = aux;
-        //--------------------------------------------------------
-        aux = props.getProperty(PROP_DATABASE_ENVIRONMENT);
-        if (aux == null) System.out.println("Warning: Property " + PROP_DATABASE_ENVIRONMENT + " Not set");
-        else AurigaObject.dbspace = aux;
-        //========================================================
     }    
-    //*****************************************************************
-    public static DataBaseName getDataBaseName () {
-        DataBaseName dbname = new DataBaseName();
-        dbname.auth = AurigaObject.dbauth;
-        dbname.project = AurigaObject.dbauth;
-        dbname.billing = AurigaObject.dbbilling;
-        dbname.universe = AurigaObject.dbuniverse;
-        dbname.maps = AurigaObject.dbmaps;
-        dbname.design = AurigaObject.dbdesign;
-        dbname.sample = AurigaObject.dbsample;
-        dbname.trial = AurigaObject.dbspace;
-        return dbname;
-    }
     //*****************************************************************
 }
 //**************************************************************************
